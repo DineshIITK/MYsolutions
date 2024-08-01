d147b805e923db3ca461c547126863376e7a91ca
DineshIITK <dinesh23@iitk.ac.in>
2024-08-01 15:18:40 +0530

    Add files via upload

---
+++ Solutions.R
@@ -0,0 +1,46 @@
+#Worksheet 2 
+#Question1
+
+factl  <- function(n)
+{
+  if(n==0)
+  {
+    return(1)
+  }
+  if(n==1)
+  {
+    return(1)
+  }
+  return(n*factl(n-1))
+}
+factl(5)
+
+
+
+#Question2
+
+EulerNo <- function(n)
+{
+  if(n<=0){
+    return("Not Valid")
+  }
+  return((1 + 1/n)**n)
+}
+
+#Question3
+
+seat <- read.csv("https://dvats.github.io/assets/course/mth208/seating.csv")
+
+SeatNo = function(roll)
+{
+  row_no = seat[seat$Roll.No == roll , ]
+  if(roll<seat[1,1]){
+    return("Invalid")
+  }
+  
+  return(row_no$Seat.Number)
+}
+SeatNo(230374)
+
+
+
